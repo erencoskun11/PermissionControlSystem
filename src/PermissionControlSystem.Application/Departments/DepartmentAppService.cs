e894ba3a1ed37d05b5be40df3cb93a92e0737bf3
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2026-01-19 23:08:55 +0300

    feat(AppService) : created

---
+++ src/PermissionControlSystem.Application/Departments/DepartmentAppService.cs
@@ -0,0 +1,27 @@
+﻿
+using PermissionControlSystem.Departments.Dtos;
+using PermissionControlSystem.Departments2;
+using System;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace PermissionControlSystem.Departments
+{
+    public class DepartmentAppService :
+        CrudAppService<
+            Department,                 // 1. Hangi Entity?
+            DepartmentDto,              // 2. Detayda ne göstereyim?
+            Guid,                       // 3. ID tipi ne?
+            PagedAndSortedResultRequestDto, // 4. Listeleme ayarları
+            CreateDepartmentDto,        // 5. Oluştururken ne isteyeyim?
+            UpdateDepartmentDto>,       // 6. Güncellerken ne isteyeyim?
+        IDepartmentAppService           // Hangi arayüzü uyguluyorum?
+    {
+        public DepartmentAppService(IRepository<Department, Guid> repository)
+            : base(repository)
+        {
+            // Burası boş kalabilir, CrudAppService her şeyi bizim için yapıyor!
+        }
+    }
+}
