60e6a16e1ee8375ff9b0c19f2b06179f11375a3a
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2026-01-19 23:11:56 +0300

    feat(mapper) : created

---
+++ src/PermissionControlSystem.Application/PermissionControlSystemApplicationAutoMapperProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using PermissionControlSystem.Departments;
+using PermissionControlSystem.Departments.Dtos;
+using PermissionControlSystem.Departments2;
+using PermissionControlSystem.Leaves;
+using PermissionControlSystem.Leaves.Dtos;
+using PermissionControlSystem.Leaves2;
+
+namespace PermissionControlSystem;
+
+public class PermissionControlSystemApplicationAutoMapperProfile : Profile
+{
+    public PermissionControlSystemApplicationAutoMapperProfile()
+    {
+        /* 1. Departman Eşleşmeleri */
+        CreateMap<Department, DepartmentDto>();
+        CreateMap<Department, DepartmentListDto>();
+        CreateMap<CreateDepartmentDto, Department>();
+        CreateMap<UpdateDepartmentDto, Department>();
+
+        /* 2. İzin Talebi (LeaveRequest) Eşleşmeleri */
+        CreateMap<LeaveRequest, LeaveRequestDto>();
+        CreateMap<LeaveRequest, LeaveRequestListDto>();
+        CreateMap<CreateLeaveRequestDto, LeaveRequest>();
+        CreateMap<UpdateLeaveRequestDto, LeaveRequest>();
+    }
+}
