500a916801e4ed8744e6ffb4650981a8ab466fef
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2026-01-21 22:49:02 +0300

    feat(test) : EmailSender

---
+++ test/PermissionControlSystem.Application.Tests/Mocks/FakeEmailSender.cs
@@ -0,0 +1,62 @@
+﻿
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Emailing;
+
+namespace PermissionControlSystem.Mocks
+{
+    // Testlerde IEmailSender istendiğinde bu sınıf devreye girecek
+    [Dependency(ReplaceServices = true)]
+    public class FakeEmailSender : IEmailSender, ITransientDependency
+    {
+        public Task SendAsync(string to, string subject, string body, bool isBodyHtml = true)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task SendAsync(string from, string to, string subject, string body, bool isBodyHtml = true)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task QueueAsync(string to, string subject, string body, bool isBodyHtml = true)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task QueueAsync(string from, string to, string subject, string body, bool isBodyHtml = true)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task QueueAsync(string to, string subject, string body, bool isBodyHtml = true, AdditionalEmailSendingArgs additionalEmailSendingArgs = null)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task QueueAsync(string from, string to, string subject, string body, bool isBodyHtml = true, AdditionalEmailSendingArgs additionalEmailSendingArgs = null)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task SendAsync(string to, string subject, string body, bool isBodyHtml = true, AdditionalEmailSendingArgs additionalEmailSendingArgs = null)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task SendAsync(string from, string to, string subject, string body, bool isBodyHtml = true, AdditionalEmailSendingArgs additionalEmailSendingArgs = null)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task SendAsync(MailMessage mail, bool normalize = true)
+        {
+            return Task.CompletedTask;
+        }
+
+
+
+
+    }
+}
