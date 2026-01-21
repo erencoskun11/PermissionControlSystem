c84a7d46075a69c3b4ce68e61cbfe0ec6f60cfdb
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2026-01-21 22:52:02 +0300

    feat(test) : rabbitmqIntegration

---
+++ test/PermissionControlSystem.Application.Tests/RabbitMqIntegrationTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using PermissionControlSystem.Events;
+using Shouldly;
+using Volo.Abp.EventBus.Distributed;
+using Xunit;
+using Microsoft.Extensions.DependencyInjection; 
+
+namespace PermissionControlSystem;
+
+[Collection(PermissionControlSystemTestConsts.CollectionDefinitionName)]
+public class RabbitMqIntegrationTest : PermissionControlSystemApplicationTestBase<PermissionControlSystemApplicationTestModule>
+{
+    private readonly IDistributedEventBus _distributedEventBus;
+
+    public RabbitMqIntegrationTest()
+    {
+        _distributedEventBus = GetRequiredService<IDistributedEventBus>();
+    }
+
+    [Fact]
+    public async Task Should_Connect_And_Publish_To_RabbitMq()
+    {
+        // 
+        var testEvent = new LeaveApprovedEto
+        {
+            LeaveRequestId = Guid.NewGuid(),
+            ManagerResponse = "TEST OTOMASYONU: RabbitMQ Bağlantı Kontrolü 🐇",
+            ApproverId = Guid.NewGuid()
+        };
+
+        // Eylem & Doğrulama
+        // Eğer RabbitMQ kapalıysa PublishAsync metodu hata fırlatır ve test başarısız olur.
+        await _distributedEventBus.PublishAsync(testEvent);
+
+        // Buraya kadar geldiyse bağlantı başarılıdır.
+        testEvent.ShouldNotBeNull();
+    }
+}
