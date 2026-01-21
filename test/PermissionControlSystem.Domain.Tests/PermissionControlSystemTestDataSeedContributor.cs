168965c872553ec1bbfd0dc736bf0e8ca5cf4886
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2026-01-21 23:02:54 +0300

    feat():created

---
+++ test/PermissionControlSystem.Domain.Tests/PermissionControlSystemTestDataSeedContributor.cs
@@ -0,0 +1,50 @@
+﻿using PermissionControlSystem.Departments2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
+
+namespace PermissionControlSystem
+{
+    public class PermissionControlSystemTestDataSeedContributor : IDataSeedContributor, ITransientDependency
+    {
+        private readonly IRepository<Department, Guid> _departmentRepository;
+
+
+        public PermissionControlSystemTestDataSeedContributor(IRepository<Department,Guid> departmentRepository)
+        {
+            _departmentRepository = departmentRepository;
+        }
+
+
+        public async Task SeedAsync(DataSeedContext context)
+        {
+            if (await _departmentRepository.GetCountAsync() <= 0)
+            {
+                await _departmentRepository.InsertAsync(
+                    new Department(
+                        Guid.NewGuid(),
+                        "Human Resources",
+                        "All HR related matters"
+                        )
+                    );
+
+                await _departmentRepository.InsertAsync(
+                    new Department(
+                        Guid.NewGuid(),
+                        "IT",
+                        "Information Technology Department"
+                        )
+
+                    );
+
+
+            }
+        }
+
+
+    }
+}
