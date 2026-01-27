68ddb04a9ce344427229958e64d8feee1a37cf15
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2026-01-27 23:40:52 +0300

    created

---
+++ src/PermissionControlSystem.Application.Contracts/Departments/IDepartmentAppService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using PermissionControlSystem.Departments.Dtos;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace PermissionControlSystem.Departments
+{
+    public interface IDepartmentAppService :
+        ICrudAppService<
+            DepartmentDto,
+            Guid,
+            PagedAndSortedResultRequestDto,
+            CreateDepartmentDto,
+            UpdateDepartmentDto>
+    {
+        // Not: Listeleme için otomatik DepartmentDto kullanılır, 
+        // özelleştirmek isterseniz GetListAsync'i override edeceğiz.
+    }
+}
