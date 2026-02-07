7d30276bf4224795ac184d52da53cc0e439674ec
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2026-02-07 22:01:18 +0300

    feat(dto): created

---
+++ src/PermissionControlSystem.Application.Contracts/Departments/Validators/CreateDepartmentDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace PermissionControlSystem.Departments.Dtos
+{
+    public class CreateDepartmentDtoValidator : AbstractValidator<CreateDepartmentDto>
+    {
+        public CreateDepartmentDtoValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Departman adı zorunludur.")
+                .MaximumLength(100).WithMessage("Departman adı 100 karakteri geçemez.");
+
+            RuleFor(x => x.Description)
+                .MaximumLength(500).WithMessage("Açıklama çok uzun.");
+        }
+    }
+}
