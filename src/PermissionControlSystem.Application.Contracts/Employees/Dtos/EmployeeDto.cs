c1bb5a5dcaf34bc956d7df81f99928ceb8dfc6a6
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2026-02-07 22:03:50 +0300

    feat(employeedto):created

---
+++ src/PermissionControlSystem.Application.Contracts/Employees/Dtos/EmployeeDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Application.Dtos;
+
+namespace PermissionControlSystem.Employees.Dtos
+{
+    public class EmployeeDto : EntityDto<Guid>
+    {
+        public Guid UserId { get; set; }
+        public Guid DepartmentId { get; set; }
+
+        public string FullName { get; set; } = null!;
+        public string? Position { get; set; }
+
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public string? DepartmentName { get; set; }
+    }
+}
