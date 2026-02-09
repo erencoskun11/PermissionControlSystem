67cc4c68cc4db3a3d48923eb992f5322e00fbc2f
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2026-02-09 17:55:28 +0300

    feat(dto):  created

---
+++ src/PermissionControlSystem.Application.Contracts/Leaves/Dtos/CreateLeaveRequestDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PermissionControlSystem.Leave.Dtos
+{
+    public class CreateLeaveRequestDto
+    {
+        // 👇 BUNU EKLE (Artık ön yüzden personel ID'si isteyeceğiz)
+        [Required]
+        public Guid EmployeeId { get; set; }
+
+        [Required]
+        public Guid LeaveTypeId { get; set; } 
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        public string Reason { get; set; }
+    }
+}
