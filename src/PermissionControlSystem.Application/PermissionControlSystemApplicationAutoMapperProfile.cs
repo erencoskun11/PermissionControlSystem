70182af19d8ee0051ac2221f58a83862b5a0b6dc
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2026-02-09 17:50:19 +0300

    updated

--- src/PermissionControlSystem.Application/PermissionControlSystemApplicationAutoMapperProfile.cs
+++ src/PermissionControlSystem.Application/PermissionControlSystemApplicationAutoMapperProfile.cs
@@ -1,12 +1,13 @@
 ﻿using AutoMapper;
 using PermissionControlSystem.AppUsers.Dtos;
+using PermissionControlSystem.Departments;
 using PermissionControlSystem.Departments.Dtos;
-using PermissionControlSystem.Departments2;
 using PermissionControlSystem.Employees;
 using PermissionControlSystem.Employees.Dtos;
+using PermissionControlSystem.Enums;
+using PermissionControlSystem.Leave;
+using PermissionControlSystem.Leave.Dtos;
 using PermissionControlSystem.Leaves;
-using PermissionControlSystem.Leaves.Dtos;
-using PermissionControlSystem.Leaves2;
 using PermissionControlSystem.Users;
 using System;
 
@@ -16,59 +17,43 @@ namespace PermissionControlSystem
     {
         public PermissionControlSystemApplicationAutoMapperProfile()
         {
-            /* --- Departmanlar --- */
+            // --- Departmanlar ---
             CreateMap<Department, DepartmentDto>();
             CreateMap<Department, DepartmentListDto>();
             CreateMap<CreateDepartmentDto, Department>();
             CreateMap<UpdateDepartmentDto, Department>();
 
-            /* --- İzinler (Leaves) --- */
-            // 1. Entity -> DTO
+            // --- İzinler (Leaves) ---
             CreateMap<LeaveRequest, LeaveRequestDto>();
             CreateMap<LeaveRequest, LeaveRequestListDto>();
 
-            // 2. Create -> Entity
             CreateMap<CreateLeaveRequestDto, LeaveRequest>()
                 .ForMember(dest => dest.Status, opt => opt.MapFrom(src => LeaveRequestStatus.Pending))
                 .ForMember(dest => dest.ManagerResponse, opt => opt.Ignore());
 
-            // 3. Update -> Entity
             CreateMap<UpdateLeaveRequestDto, LeaveRequest>();
 
-            /* --- AppUsers (Eğer kullanıyorsan) --- */
-            // Not: AppUserDto ve diğerleri tanımlıysa burası kalabilir.
-            // Eğer yoksa hata verir, proje içinde kontrol et.
-            // CreateMap<AppUser, AppUserDto>();
-            // CreateMap<CreateAppUserDto, AppUser>();
-            // CreateMap<UpdateAppUserDto, AppUser>();
+            // --- AppUsers ---
+            CreateMap<AppUser, AppUserDto>();
 
-            /* --- Employees (ÇALIŞANLAR) --- */
-
-            // 1. Entity -> DTO (Listeleme)
-            // Department null gelirse hata vermemesi için kontrol ekli
+            // --- Employees ---
             CreateMap<Employee, EmployeeDto>()
                 .ForMember(dest => dest.DepartmentName,
                     opt => opt.MapFrom(src => src.Department != null ? src.Department.Name : null));
 
-            // Not: EmployeeListDto diye bir dosya oluşturmadıysak 
-            // aşağıdaki bloğu silebilirsin veya EmployeeDto kullanabilirsin.
-            // CreateMap<Employee, EmployeeListDto>() ...
-
-            // 2. Create -> Entity (Kayıt)
-            // 👇 KRİTİK DÜZELTME BURADA 👇
             CreateMap<CreateEmployeeDto, Employee>()
                 .ConstructUsing(src => new Employee(
-                    Guid.NewGuid(),      // ID
-                    src.UserId,          // User ID
-                    src.DepartmentId,    // Dept ID
-                    src.FullName,        // Ad Soyad
-                    src.Email,           // Email
-                    src.PhoneNumber      // Tel
+                    Guid.NewGuid(),      // 1. Id
+                    src.UserId,          // 2. UserId
+                    src.DepartmentId,    // 3. DepartmentId
+                    src.FirstName,       // 4. FirstName
+                    src.LastName,        // 5. LastName (Eksik olan buydu)
+                    src.Email,           // 6. Email
+                    src.PhoneNumber,       // 7. PhoneNumber
+                    src.Position         // 7. Position
                 ))
-                // Position, Constructor'da olmadığı için onu ayrıca setliyoruz:
-                .ForMember(dest => dest.Position, opt => opt.MapFrom(src => src.Position));
+                .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber));
 
-            // 3. Update -> Entity (Güncelleme)
             CreateMap<UpdateEmployeeDto, Employee>();
         }
     }
