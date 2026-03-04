ed46bb0f22909a643f60d9d4d870c32d42650970
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2026-03-04 17:40:18 +0300

    feat() : created for signalR

---
+++ src/PermissionControlSystem.Application/SignalR/NotificationHub.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.AspNetCore.SignalR;
+
+namespace PermissionControlSystem.SignalR
+{
+    public class NotificationHub : AbpHub
+    {
+
+    }
+}
