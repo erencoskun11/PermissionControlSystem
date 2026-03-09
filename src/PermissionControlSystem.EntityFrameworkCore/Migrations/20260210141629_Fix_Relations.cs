c1150efc657deb90188e46ca4d75aea66dd16965
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2026-03-09 17:19:51 +0300

    migration

---
+++ src/PermissionControlSystem.EntityFrameworkCore/Migrations/20260210141629_Fix_Relations.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PermissionControlSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fix_Relations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_AppEmployees_UserId",
+                table: "AppEmployees",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AppEmployees_UserId",
+                table: "AppEmployees");
+        }
+    }
+}
