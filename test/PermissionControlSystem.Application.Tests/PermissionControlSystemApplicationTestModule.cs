2bc04c287a5a5ae6d5fd576f487d435a04b19627
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2026-03-22 16:19:19 +0300

    created

--- test/PermissionControlSystem.Application.Tests/PermissionControlSystemApplicationTestModule.cs
+++ test/PermissionControlSystem.Application.Tests/PermissionControlSystemApplicationTestModule.cs
@@ -1,7 +1,18 @@
 ﻿using Microsoft.Extensions.Caching.StackExchangeRedis;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using OpenSearch.Client;
+using PermissionControlSystem.Enums;
+using PermissionControlSystem.Models;
+using PermissionControlSystem.Services;
+using System;
+using System.Threading.Tasks;
 using Volo.Abp.Caching;
 using Volo.Abp.Caching.StackExchangeRedis;
-using Volo.Abp.EventBus.RabbitMq; 
+using Volo.Abp.Emailing;
+using Volo.Abp.EventBus.RabbitMq;
 using Volo.Abp.Modularity;
 using Volo.Abp.RabbitMQ;
 
@@ -10,27 +21,28 @@ namespace PermissionControlSystem;
 [DependsOn(
     typeof(PermissionControlSystemApplicationModule),
     typeof(PermissionControlSystemDomainTestModule),
-    typeof(AbpEventBusRabbitMqModule) ,
-    typeof(AbpCachingStackExchangeRedisModule) 
-    )]
+    typeof(AbpEventBusRabbitMqModule),
+    typeof(AbpCachingStackExchangeRedisModule)
+)]
 public class PermissionControlSystemApplicationTestModule : AbpModule
 {
     public override void ConfigureServices(ServiceConfigurationContext context)
     {
-        // 2. RabbitMQ Bağlantı Ayarlarını Test İçin Zorla
         Configure<AbpRabbitMqOptions>(options =>
         {
-            options.Connections.Default.HostName = "localhost";
-            options.Connections.Default.UserName = "guest";
-            options.Connections.Default.Password = "guest";
+            options.Connections.Default.HostName = "46.224.57.16";
+            options.Connections.Default.Port = 5672;
+            options.Connections.Default.UserName = "admin";
+            options.Connections.Default.Password = "thescappe.123";
+            options.Connections.Default.VirtualHost = "eren";
         });
 
-        // Testlerde Event Bus'ın gerçek RabbitMQ olmasını sağla
         Configure<AbpRabbitMqEventBusOptions>(options =>
         {
             options.ClientName = "Test_Client";
             options.ExchangeName = "Test_Exchange";
         });
+
         Configure<AbpDistributedCacheOptions>(options =>
         {
             options.KeyPrefix = "PermissionSystemTest:";
@@ -38,12 +50,31 @@ public class PermissionControlSystemApplicationTestModule : AbpModule
 
         Configure<RedisCacheOptions>(options =>
         {
-            // Docker'da veya yerelde Redis genelde bu portta çalışır
-            options.Configuration = "127.0.0.1:6379";
+            // Test ortamında farklı bir veritabanı (DB 10) kullanıyoruz ki gerçek verilerle karışmasın
+            options.Configuration = "46.224.57.16:6379,password=thescappe.123,defaultDatabase=10,abortConnect=false";
         });
 
+        // Email gönderimini testlerde susturuyoruz (Sahte gönderici)
+        context.Services.Replace(ServiceDescriptor.Singleton<IEmailSender>(Substitute.For<IEmailSender>()));
 
+        // Elasticsearch servisini "Dummy" olanla değiştiriyoruz
+        context.Services.Replace(ServiceDescriptor.Transient<ElasticSearchService, DummyElasticSearchService>());
+    }
+}
 
+// 🔥 Dummy Servis: Gerçek bir arama motoruna ihtiyaç duymadan testlerin akmasını sağlar
+public class DummyElasticSearchService : ElasticSearchService
+{
+    public DummyElasticSearchService()
+        : base(
+            Substitute.For<IOpenSearchClient>(),
+            Substitute.For<ILogger<ElasticSearchService>>() // 👈 Logger eklendi!
+        )
+    { }
 
+    // Status parametreli yeni imza (Interface ile uyumlu)
+    public override Task IndexLeaveRequestAsync(LeaveIndexModel model)
+    {
+        return Task.CompletedTask;
     }
 }
