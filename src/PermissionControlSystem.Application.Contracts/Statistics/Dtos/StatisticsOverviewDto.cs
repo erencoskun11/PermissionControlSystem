3c871380b327d6f4ff42b8d10af650dd624e6b89
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2026-03-25 17:35:14 +0300

    created

---
+++ src/PermissionControlSystem.Application.Contracts/Statistics/Dtos/StatisticsOverviewDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PermissionControlSystem.Statistics.Dtos
+{
+    public class StatisticsOverviewDto
+    {
+        public int TotalRequests { get; set; }
+        public int ApprovedRequests { get; set; }
+        public int RejectedRequests { get; set; }
+        public int TotalLeaveDays { get; set; }
+    }
+}
