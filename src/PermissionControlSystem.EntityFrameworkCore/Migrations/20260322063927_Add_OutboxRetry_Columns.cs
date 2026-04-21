b06d062dbecb9d45b0e9e68e0ad4d51d377051a8
Eren Coskun <eren1coskun11@gmail.com>
2026-04-21 17:27:41 +0300

    migration

---
+++ src/PermissionControlSystem.EntityFrameworkCore/Migrations/20260322063927_Add_OutboxRetry_Columns.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PermissionControlSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_OutboxRetry_Columns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FailedOn",
+                table: "OutboxMessages",
+                type: "timestamp without time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastError",
+                table: "OutboxMessages",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RetryCount",
+                table: "OutboxMessages",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FailedOn",
+                table: "OutboxMessages");
+
+            migrationBuilder.DropColumn(
+                name: "LastError",
+                table: "OutboxMessages");
+
+            migrationBuilder.DropColumn(
+                name: "RetryCount",
+                table: "OutboxMessages");
+        }
+    }
+}
