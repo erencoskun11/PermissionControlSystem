1ae10a4b38a98cdbeeefaaadc49435ec8687c3df
Eren Coskun <eren1coskun11@gmail.com>
2026-04-21 17:24:28 +0300

    migration

---
+++ src/PermissionControlSystem.EntityFrameworkCore/Migrations/20260211111526_Fix_Relationships_V2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PermissionControlSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fix_Relationships_V2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AppLeaveRequests_AppEmployees_EmployeeId1",
+                table: "AppLeaveRequests");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AppLeaveRequests_EmployeeId1",
+                table: "AppLeaveRequests");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeId1",
+                table: "AppLeaveRequests");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "EmployeeId1",
+                table: "AppLeaveRequests",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppLeaveRequests_EmployeeId1",
+                table: "AppLeaveRequests",
+                column: "EmployeeId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AppLeaveRequests_AppEmployees_EmployeeId1",
+                table: "AppLeaveRequests",
+                column: "EmployeeId1",
+                principalTable: "AppEmployees",
+                principalColumn: "Id");
+        }
+    }
+}
