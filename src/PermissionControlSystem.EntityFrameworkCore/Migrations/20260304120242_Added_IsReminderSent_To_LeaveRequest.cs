496948edbf52c71dd06f1988dbb01dfcbb87f837
Eren Coskun <eren1coskun11@gmail.com>
2026-04-21 17:23:57 +0300

    migration

---
+++ src/PermissionControlSystem.EntityFrameworkCore/Migrations/20260304120242_Added_IsReminderSent_To_LeaveRequest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PermissionControlSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_IsReminderSent_To_LeaveRequest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsReminderSent",
+                table: "AppLeaveRequests",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsReminderSent",
+                table: "AppLeaveRequests");
+        }
+    }
+}
