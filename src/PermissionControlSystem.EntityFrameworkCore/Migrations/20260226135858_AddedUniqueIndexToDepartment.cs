6bab328420332939c097316b4e99d0dd4813e0f6
Eren Coskun <eren1coskun11@gmail.com>
2026-04-21 17:25:38 +0300

    migration

---
+++ src/PermissionControlSystem.EntityFrameworkCore/Migrations/20260226135858_AddedUniqueIndexToDepartment.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PermissionControlSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedUniqueIndexToDepartment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_AppDepartments_Name",
+                table: "AppDepartments",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AppDepartments_Name",
+                table: "AppDepartments");
+        }
+    }
+}
