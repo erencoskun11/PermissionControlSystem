a4c3904be9accec4544189b68184983360afc9e0
Eren Coskun <eren1coskun11@gmail.com>
2026-04-21 17:28:14 +0300

    migration

---
+++ src/PermissionControlSystem.EntityFrameworkCore/Migrations/20260304135739_Added_OutboxMessage_Entity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PermissionControlSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_OutboxMessage_Entity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OutboxMessages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    OccurredOn = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    ProcessedOn = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    ExtraProperties = table.Column<string>(type: "text", nullable: false),
+                    ConcurrencyStamp = table.Column<string>(type: "character varying(40)", maxLength: 40, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OutboxMessages", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OutboxMessages");
+        }
+    }
+}
