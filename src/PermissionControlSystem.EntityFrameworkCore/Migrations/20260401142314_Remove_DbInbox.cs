0fbfd28bda42099bc25f335010ee20d24f6bd5da
Eren Coskun <eren1coskun11@gmail.com>
2026-04-27 17:28:32 +0300

    migration

---
+++ src/PermissionControlSystem.EntityFrameworkCore/Migrations/20260401142314_Remove_DbInbox.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PermissionControlSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class Remove_DbInbox : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AbpEventInbox");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AbpEventInbox",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    EventData = table.Column<byte[]>(type: "bytea", nullable: false),
+                    EventName = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    ExtraProperties = table.Column<string>(type: "text", nullable: false),
+                    HandledTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    MessageId = table.Column<string>(type: "text", nullable: false),
+                    NextRetryTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    RetryCount = table.Column<int>(type: "integer", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AbpEventInbox", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpEventInbox_MessageId",
+                table: "AbpEventInbox",
+                column: "MessageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpEventInbox_Status_CreationTime",
+                table: "AbpEventInbox",
+                columns: new[] { "Status", "CreationTime" });
+        }
+    }
+}
