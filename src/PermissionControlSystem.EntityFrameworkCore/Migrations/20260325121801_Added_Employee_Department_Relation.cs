3bd08b99dedeca52f8779f272700b1b6722c21a6
Eren Coskun <eren1coskun11@gmail.com>
2026-04-27 17:27:51 +0300

    migration

---
+++ src/PermissionControlSystem.EntityFrameworkCore/Migrations/20260325121801_Added_Employee_Department_Relation.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PermissionControlSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_Employee_Department_Relation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
