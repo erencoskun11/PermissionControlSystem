4a1aaba4d53c516ffe3890cfde46ae9fa4c3d0db
Eren Coskun <eren1coskun11@gmail.com>
2026-04-27 17:27:15 +0300

    migration

---
+++ src/PermissionControlSystem.EntityFrameworkCore/Migrations/20260401075628_Added_ConcurrencyStamp_To_LeaveRequest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PermissionControlSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_ConcurrencyStamp_To_LeaveRequest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ConcurrencyStamp",
+                table: "AppLeaveRequests",
+                type: "character varying(40)",
+                maxLength: 40,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExtraProperties",
+                table: "AppLeaveRequests",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConcurrencyStamp",
+                table: "AppLeaveRequests");
+
+            migrationBuilder.DropColumn(
+                name: "ExtraProperties",
+                table: "AppLeaveRequests");
+        }
+    }
+}
