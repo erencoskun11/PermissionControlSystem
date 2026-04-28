9766eaaae10b92904ac1d864a9683b6634647862
Eren Coskun <eren1coskun11@gmail.com>
2026-04-28 22:57:45 +0300

    feat():created

---
+++ src/PermissionControlSystem.Domain.Shared/Events/DepartmentCreatedEto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.EventBus;
+
+namespace PermissionControlSystem.Events
+{
+    [EventName("PermissionControlSystem.Department.Created")]
+    public class DepartmentCreatedEto
+    {
+        public Guid EventId { get; set; } = Guid.NewGuid(); // 🔥 BÖYLECE MESAJIN KİMLİĞİ OLACAK
+        public Guid DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+        public string Message { get; set; }
+    }
+}
