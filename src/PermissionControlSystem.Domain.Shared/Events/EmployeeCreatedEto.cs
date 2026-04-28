562cfb86dce9e3a69bb073d7b0e9cff9759d67dc
Eren Coskun <eren1coskun11@gmail.com>
2026-04-28 22:59:42 +0300

    feat():created

---
+++ src/PermissionControlSystem.Domain.Shared/Events/EmployeeCreatedEto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PermissionControlSystem.Events
+{
+    public class EmployeeCreatedEto
+    {
+        public Guid EventId { get; set; } = Guid.NewGuid(); 
+        public Guid EmployeeId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Position { get; set; }
+    }
+}
