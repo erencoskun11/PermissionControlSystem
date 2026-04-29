8f6395987a1a86b70567a501eb8daea048f6e2ba
Eren Coskun <eren1coskun11@gmail.com>
2026-04-29 17:45:14 +0300

    feat(dto) : created

--- src/PermissionControlSystem.Application.Contracts/Employees/Dtos/EmployeeDto.cs
+++ src/PermissionControlSystem.Application.Contracts/Employees/Dtos/EmployeeDto.cs
@@ -1,22 +1,25 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Volo.Abp.Application.Dtos;
+using Volo.Abp.Domain.Entities;
 
 namespace PermissionControlSystem.Employees.Dtos
 {
-    public class EmployeeDto : EntityDto<Guid>
+    public class EmployeeDto : EntityDto<Guid>, IHasConcurrencyStamp
     {
         public Guid UserId { get; set; }
         public Guid DepartmentId { get; set; }
 
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string FullName { get; set; } 
         public string? Position { get; set; }
 
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
 
         public string? DepartmentName { get; set; }
+        public string ConcurrencyStamp { get; set; } = string.Empty;
     }
 }
