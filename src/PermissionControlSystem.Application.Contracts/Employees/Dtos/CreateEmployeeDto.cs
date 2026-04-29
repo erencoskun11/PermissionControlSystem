e2cc7f29f2e9929e6e0987067c974c08ba9d6220
Eren Coskun <eren1coskun11@gmail.com>
2026-04-29 17:46:18 +0300

    feat() : created

--- src/PermissionControlSystem.Application.Contracts/Employees/Dtos/CreateEmployeeDto.cs
+++ src/PermissionControlSystem.Application.Contracts/Employees/Dtos/CreateEmployeeDto.cs
@@ -7,25 +7,18 @@ namespace PermissionControlSystem.Employees.Dtos
 {
     public class CreateEmployeeDto
     {
-        [Required]
         public Guid UserId { get; set; }
 
-        [Required]
         public Guid DepartmentId { get; set; }
 
-        [Required]
-        [StringLength(150)]
-        public string FirstName { get; set; } = null!;
+        public string FirstName { get; set; } 
 
         public string LastName { get; set; }
         public string? Position { get; set; }
+        public string FullName { get; set; }
 
-        [Required]
-        [EmailAddress]
-        public string? Email { get; set; }
+        public string Email { get; set; }
 
-        [Required]
-        [Phone]
-        public string? PhoneNumber { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
