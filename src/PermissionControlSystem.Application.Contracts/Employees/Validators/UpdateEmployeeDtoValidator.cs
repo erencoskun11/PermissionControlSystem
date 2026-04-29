7999374844b0a8f35854b058f790fea31a13f5c6
Eren Coskun <eren1coskun11@gmail.com>
2026-04-29 17:47:50 +0300

    feat() : created

---
+++ src/PermissionControlSystem.Application.Contracts/Employees/Validators/UpdateEmployeeDtoValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using PermissionControlSystem.Employees.Dtos;
+
+namespace PermissionControlSystem.Employees.Validators
+{
+    public class UpdateEmployeeDtoValidator : AbstractValidator<UpdateEmployeeDto>
+    {
+        public UpdateEmployeeDtoValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty().WithMessage("Personel adı boş bırakılamaz.")
+                .MaximumLength(50).WithMessage("Ad 50 karakterden uzun olamaz.");
+
+            RuleFor(x => x.LastName)
+                .NotEmpty().WithMessage("Personel soyadı boş bırakılamaz.")
+                .MaximumLength(50).WithMessage("Soyad 50 karakterden uzun olamaz.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("E-Posta adresi zorunludur.")
+                .EmailAddress().WithMessage("Lütfen geçerli bir e-posta adresi giriniz.");
+
+            RuleFor(x => x.PhoneNumber)
+                .NotEmpty().WithMessage("Telefon numarası boş bırakılamaz.");
+
+            RuleFor(x => x.Position)
+                .NotEmpty().WithMessage("Pozisyon / Unvan bilgisi boş bırakılamaz.");
+        }
+    }
+}
