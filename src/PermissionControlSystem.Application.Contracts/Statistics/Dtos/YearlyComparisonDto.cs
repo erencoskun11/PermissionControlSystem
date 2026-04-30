dffb92ec7e07379f4bf61c39c614e3e527dd6d93
Eren Coskun <eren1coskun11@gmail.com>
2026-04-30 16:57:57 +0300

    feat(dto) : created

---
+++ src/PermissionControlSystem.Application.Contracts/Statistics/Dtos/YearlyComparisonDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PermissionControlSystem.Statistics.Dtos
+{
+    public class YearlyComparisonDto
+    {
+        public int PreviousYearTotal { get; set; }
+        public int CurrentYearTotal { get; set; }
+        public double ChangeRate { get; set; }
+    }
+}
