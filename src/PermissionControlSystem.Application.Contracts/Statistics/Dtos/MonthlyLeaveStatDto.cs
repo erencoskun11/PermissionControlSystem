b453eacdbada18339269ebbbcb69c5aa74058ab5
Eren Coskun <eren1coskun11@gmail.com>
2026-04-30 16:59:48 +0300

    feat () : created

---
+++ src/PermissionControlSystem.Application.Contracts/Statistics/Dtos/MonthlyLeaveStatDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PermissionControlSystem.Statistics.Dtos
+{
+    public class MonthlyLeaveStatDto
+    {
+        public string MonthName { get; set; }
+        public int TotalDays { get; set; }
+    }
+}
