5ce59603d3e83e0f12e2a9ea4f8a0423a776d3f6
Eren Coskun <eren1coskun11@gmail.com>
2026-04-30 16:58:59 +0300

    feat() : created

---
+++ src/PermissionControlSystem.Application.Contracts/Statistics/Dtos/TopEmployeeStatDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PermissionControlSystem.Statistics.Dtos
+{
+    public class TopEmployeeStatDto
+    {
+        public string EmployeeName { get; set; }
+        public string DepartmentName { get; set; }
+        public int RequestCount { get; set; }
+        public int TotalDays { get; set; }
+    }
+}
