292b39f445daeaa0ba362898fb9d643d97dabc90
Eren Coskun <eren1coskun11@gmail.com>
2026-05-01 23:59:16 +0300

    dto

--- src/PermissionControlSystem.Application.Contracts/Departments/Dtos/UpdateDepartmentDto.cs
+++ src/PermissionControlSystem.Application.Contracts/Departments/Dtos/UpdateDepartmentDto.cs
@@ -1,13 +1,15 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
+using Volo.Abp.Domain.Entities;
 
 namespace PermissionControlSystem.Departments.Dtos
 {
-    public class UpdateDepartmentDto
+    public class UpdateDepartmentDto : IHasConcurrencyStamp
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ConcurrencyStamp { get; set; } = string.Empty;
     }
 }
