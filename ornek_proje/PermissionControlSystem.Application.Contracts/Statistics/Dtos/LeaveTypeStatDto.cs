ed8a9cd006b8a1d6fbae204ffc67b72a37960a02
Eren Coskun <eren1coskun11@gmail.com>
2026-05-04 17:49:24 +0300

    feat() : created

---
+++ ornek_proje/PermissionControlSystem.Application.Contracts/Statistics/Dtos/LeaveTypeStatDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PermissionControlSystem.Statistics.Dtos
+{
+    public class LeaveTypeStatDto
+    {
+        public string LeaveTypeName { get; set; }
+        public int TotalCount { get; set; }
+    }
+}
