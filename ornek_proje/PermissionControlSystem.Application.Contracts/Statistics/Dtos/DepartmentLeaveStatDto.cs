8178680c1111668698f1c6a3a088a34dc7717da5
Eren Coskun <eren1coskun11@gmail.com>
2026-05-04 17:47:57 +0300

    feat() : created

---
+++ ornek_proje/PermissionControlSystem.Application.Contracts/Statistics/Dtos/DepartmentLeaveStatDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PermissionControlSystem.Statistics.Dtos
+{
+    public class DepartmentLeaveStatDto
+    {
+        public string DepartmentName { get; set; }
+        public int TotalDays { get; set; }
+        public int TotalRequests { get; set; }
+    }
+}
