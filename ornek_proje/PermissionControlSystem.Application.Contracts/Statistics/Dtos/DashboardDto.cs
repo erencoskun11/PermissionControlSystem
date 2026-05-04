9e3c2462970df2c87f500b910d2aaa1f5fc9b0f6
Eren Coskun <eren1coskun11@gmail.com>
2026-05-04 17:48:44 +0300

    feat(dto) : created

---
+++ ornek_proje/PermissionControlSystem.Application.Contracts/Statistics/Dtos/DashboardDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace PermissionControlSystem.Statistics.Dtos
+{
+    public class DashboardDto
+    {
+        public StatisticsOverviewDto Overview { get; set; }
+        public List<DepartmentLeaveStatDto> DepartmentStats { get; set; }
+        public List<LeaveTypeStatDto> LeaveTypeStats { get; set; }
+        public List<TopEmployeeStatDto> TopEmployees { get; set; }
+        public List<RejectedEmployeeStatDto> MostRejected { get; set; }
+        public List<MonthlyLeaveStatDto> MonthlyLeaves { get; set; }
+        public List<OldestPendingLeaveStatDto> OldestPending { get; set; }
+        public YearlyComparisonDto YearlyComparison { get; set; }
+    }
+}
