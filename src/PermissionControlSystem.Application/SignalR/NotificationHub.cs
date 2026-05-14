2f748a0601285a1a3c23e738fd3d7ef052f21fc2
Eren Coskun <eren1coskun11@gmail.com>
2026-05-14 09:04:14 +0300

    feat() : created

--- src/PermissionControlSystem.Application/SignalR/NotificationHub.cs
+++ src/PermissionControlSystem.Application/SignalR/NotificationHub.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Volo.Abp.AspNetCore.SignalR;
@@ -8,5 +10,8 @@ namespace PermissionControlSystem.SignalR
     public class NotificationHub : AbpHub
     {
 
+       //(Broadcasting)Bu sınıfa bir şey yazmamamızın sebebi, tarayıcıdan (Client) sunucuya (Server) özel bir komut göndermiyor
+        //olmamızdır. Sen sadece sunucuda bir olay olduğunda (yeni izin vb.) kullanıcılara anons yapmak istiyorsun.Mesajı Hub 
+        //içinden değil, EventHandler içinden fırlattığın için bu sınıfın içi boş kalıyor.
     }
 }
