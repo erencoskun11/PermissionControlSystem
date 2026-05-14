ce43db653bac6aa4f631ab4e807da1ac25f3381c
Eren Coskun <eren1coskun11@gmail.com>
2026-05-14 09:10:24 +0300

    feat(): created

--- src/PermissionControlSystem.Application.Contracts/Departments/IDepartmentAppService.cs
+++ src/PermissionControlSystem.Application.Contracts/Departments/IDepartmentAppService.cs
@@ -1,5 +1,11 @@
-﻿using System;
+﻿using Nest;
+using PermissionControlSystem.Caching;
 using PermissionControlSystem.Departments.Dtos;
+using PermissionControlSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
 
@@ -13,7 +19,11 @@ namespace PermissionControlSystem.Departments
             CreateDepartmentDto,
             UpdateDepartmentDto>
     {
+        Task BulkCreateAsync(List<CreateDepartmentDto> input,CancellationToken cancellationToken = default);
+
         // Not: Listeleme için otomatik DepartmentDto kullanılır, 
         // özelleştirmek isterseniz GetListAsync'i override edeceğiz.
+        Task<List<DepartmentCacheItem>> GetDepartmentsAsync(CancellationToken cancellationToken = default);
+        Task<List<DepartmentDto>> SearchFromElasticAsync(string keyword, CancellationToken cancellationToken = default);
     }
 }
