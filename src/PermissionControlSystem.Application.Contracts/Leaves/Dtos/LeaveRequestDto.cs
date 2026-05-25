afcee8c49e2ec85ff11fca2c855c4843d1b6f5e6
Eren Coskun <eren1coskun11@gmail.com>
2026-05-25 16:00:03 +0300

    feat(dto) : created

--- src/PermissionControlSystem.Application.Contracts/Leaves/Dtos/LeaveRequestDto.cs
+++ src/PermissionControlSystem.Application.Contracts/Leaves/Dtos/LeaveRequestDto.cs
@@ -2,10 +2,11 @@
 using PermissionControlSystem.Leaves;
 using System;
 using Volo.Abp.Application.Dtos;
+using Volo.Abp.Domain.Entities;
 
 namespace PermissionControlSystem.Leave.Dtos
 {
-    public class LeaveRequestDto : AuditedEntityDto<Guid>
+    public class LeaveRequestDto : AuditedEntityDto<Guid>,IHasConcurrencyStamp
     {
 
         public Guid EmployeeId { get; set; }
@@ -16,5 +17,8 @@ namespace PermissionControlSystem.Leave.Dtos
         public string Reason { get; set; }
         public LeaveRequestStatus Status { get; set; }
         public string? ManagerResponse { get; set; }
+
+        // 🔥 MÜHÜR BURADA EKRANA GİDİYOR
+        public string ConcurrencyStamp { get; set; }
     }
 }
