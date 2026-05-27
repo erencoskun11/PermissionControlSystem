bab07b6554954625e2f1320d5110da5902d3572e
Eren Coskun <eren1coskun11@gmail.com>
2026-05-27 10:21:45 +0300

    feat(interface ) : created

--- src/PermissionControlSystem.Application.Contracts/Employees/IEmployeeAppService.cs
+++ src/PermissionControlSystem.Application.Contracts/Employees/IEmployeeAppService.cs
@@ -1,18 +1,18 @@
-﻿using System;
+﻿using PermissionControlSystem.Caching;
+using PermissionControlSystem.Employees.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
-using PermissionControlSystem.Employees.Dtos; // 👈 EKSİK OLAN BU!
 
 namespace PermissionControlSystem.Employees
 {
-    public interface IEmployeeAppService :
-        ICrudAppService<
-            EmployeeDto,
-            Guid,
-            PagedAndSortedResultRequestDto,
-            CreateEmployeeDto,
-            UpdateEmployeeDto>
+    public interface IEmployeeAppService : ICrudAppService<EmployeeDto, Guid, PagedAndSortedResultRequestDto, CreateEmployeeDto, UpdateEmployeeDto>
     {
-
+        Task<List<EmployeeCacheItem>> GetCachedEmployeeListAsync(CancellationToken cancellationToken);
+        Task<List<EmployeeDto>> SearchAsync(string keyword,CancellationToken cancellationToken);
+        Task BulkCreateAsync(List<CreateEmployeeDto> input,CancellationToken cancellationToken);
     }
 }
